ee72800a20bc312d7aa819091b0c8ae90b563510
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-08-15 21:09:19 +0100

    to str

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""contains a function module"""
+
+
+def to_str(n: float) -> str:
+    """
+    Convert a float to its string representation.
+
+    Args:
+        n (float): The float number to convert.
+
+    Returns:
+        str: The string representation.
+    """
+    return str(n)
