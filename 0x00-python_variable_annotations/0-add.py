0f3fd671fa2ef85a9da4526e9ffe10d0c45d8784
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-08-15 20:46:25 +0100

    updated

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+# contains a add function
 
 def add(a: float, b: float) -> float:
+    # return a sum of floats
     return a + b
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# This module define concat class
+
+def concat(str1: str, str2: str) -> str:
+    # defines a function that reurn a concat string
+    return str1 + str2
